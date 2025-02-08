81d1d793b1084dc68a13391e7e32561097a7b3f7
shengguangming <shengguangming@bytedance.com>
2025-02-08 18:37:51 +0800

    fix

--- tests/checkpoint/test_fsdp_ckpt.py
+++ tests/checkpoint/test_fsdp_ckpt.py
@@ -27,11 +27,13 @@ from transformers import Qwen2Config
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP, ShardingStrategy, MixedPrecision, \
             CPUOffload
 
+local_rank, rank, world_size = initialize_global_process_group()
+
 
 @pytest.mark.parametrize("sharded_state_dict", [True, False])
 def test_fsdp_ckpt(sharded_state_dict):
     assert torch.cuda.device_count() >= 2, "need at least 2 gpus for test"
-    local_rank, rank, world_size = initialize_global_process_group()
+
     device_mesh = init_device_mesh('cuda', mesh_shape=(world_size,), mesh_dim_names=('dp',))
 
     model_name = 'Qwen/Qwen2.5-0.5B-Instruct'
@@ -41,6 +43,7 @@ def test_fsdp_ckpt(sharded_state_dict):
         model = AutoModelForCausalLM.from_config(config=config,
                                                  torch_dtype=torch.bfloat16,
                                                  attn_implementation='flash_attention_2')
+        model = model.to(device='cuda')
 
     # Wrap model with FSDP
     mixed_precision = MixedPrecision(param_dtype=torch.bfloat16, reduce_dtype=torch.float32, buffer_dtype=torch.float32)
