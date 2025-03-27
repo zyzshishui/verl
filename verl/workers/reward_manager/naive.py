74b7edbf841267db71266b9b9259175099be413b
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-03-27 13:18:42 +0000

    fix: remove uncessary verify from naive

--- verl/workers/reward_manager/naive.py
+++ verl/workers/reward_manager/naive.py
@@ -27,42 +27,6 @@ class NaiveRewardManager:
         self.compute_score = compute_score or _default_compute_score
         self.reward_fn_key = reward_fn_key
 
-    def verify(self, data):
-        scores = []
-        for i in range(len(data)):
-            data_item = data[i]  # DataProtoItem
-
-            prompt_ids = data_item.batch['prompts']
-
-            prompt_length = prompt_ids.shape[-1]
-
-            valid_prompt_length = data_item.batch['attention_mask'][:prompt_length].sum()
-            valid_prompt_ids = prompt_ids[-valid_prompt_length:]
-
-            response_ids = data_item.batch['responses']
-            valid_response_length = data_item.batch['attention_mask'][prompt_length:].sum()
-            valid_response_ids = response_ids[:valid_response_length]
-
-            # decode
-            prompt_str = self.tokenizer.decode(valid_prompt_ids, skip_special_tokens=True)
-            response_str = self.tokenizer.decode(valid_response_ids, skip_special_tokens=True)
-
-            ground_truth = data_item.non_tensor_batch['reward_model']['ground_truth']
-
-            data_source = data_item.non_tensor_batch[self.reward_fn_key]
-
-            extra_info = data_item.non_tensor_batch.get('extra_info', None)
-
-            score = self.compute_score(
-                data_source=data_source,
-                solution_str=response_str,
-                ground_truth=ground_truth,
-                extra_info=extra_info,
-            )
-            scores.append(score)
-        data.batch['acc'] = torch.tensor(scores, dtype=torch.float32, device=prompt_ids.device)
-        return scores
-
     def __call__(self, data: DataProto):
         """We will expand this function gradually based on the available datasets"""
 
