77f0429ab4017b94ae4a3180bdc3303ebf92a108
Xiang Long <longxiang1@modelbest.cn>
2025-04-15 08:59:11 +0000

    Try use mb cluster training

--- examples/sglang_multiturn/run_qwen2.5-7b_math_gsm8k_fsdp_multiturn.sh
+++ examples/sglang_multiturn/run_qwen2.5-7b_math_gsm8k_fsdp_multiturn.sh
@@ -1,6 +1,14 @@
 set -x
 
-gsm8k_train_path=$HOME/data/gsm8k/train.parquet
-gsm8k_test_path=$HOME/data/gsm8k/test.parquet
+export PYTHONUNBUFFERED=1
+export HF_HUB_OFFLINE=1
+export HF_ENDPOINT=https://hf-mirror.com
+export WANDB_MODE=offline
+export WANDB_DIR=logs/wandb/
+export RUST_BACKTRACE=1
+export HYDRA_FULL_ERROR=1
 
-python3 -m verl.trainer.main_ppo --config-path=/root/verl/examples/sglang_multiturn/config --config-name='gsm8k_multiturn'
+ulimit -n 65535
+python3 verl.trainer.main_ppo \
+    --config-path=/user/longxiang1/workspace/verl/examples/sglang_multiturn/config \
+    --config-name='gsm8k_multiturn'
