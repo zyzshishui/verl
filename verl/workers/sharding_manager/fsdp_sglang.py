d31b92334aa467b33906f45501b1720a4a2297ad
ocss884 <ocss.lin@gmail.com>
2025-05-28 19:29:52 -0700

    update license

--- verl/workers/sharding_manager/fsdp_sglang.py
+++ verl/workers/sharding_manager/fsdp_sglang.py
@@ -1,18 +1,5 @@
 # Copyright 2023-2024 SGLang Team
 # Copyright 2025 ModelBest Inc. and/or its affiliates
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 # Copyright 2024 Bytedance Ltd. and/or its affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,7 +16,6 @@
 
 import logging
 import os
-from typing import Union
 
 import torch
 import torch.distributed as dist
@@ -179,7 +165,7 @@ class FSDPSGLangShardingManager(BaseShardingManager):
     def release_memory(self):
         if self.device_mesh["infer_tp"].get_local_rank() == 0:
             self.inference_engine.release_memory_occupation()
-    
+
     def preprocess_data(self, data: DataProto) -> DataProto:
         """All gather across tp group to make each rank has identical input."""
         if self.tp_size == 1:
